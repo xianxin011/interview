cda6e0fbfeac839d24191fde912ee29338fe8e53
xianxin011 <1141453778@qq.com>
2026-08-25 17:57:27 +0800

    feat: add md

--- git-commit.sh
+++ git-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e  # Exit on any error
-
+# git add . && git commit -m "feat: add md" && git push origin master
 git add .
 if ! git diff --quiet --cached; then
   git commit -m "feat: add md"
